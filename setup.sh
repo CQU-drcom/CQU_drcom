b8e9a785e7f02c95b4dd2c65fd2001f55a516121
purefkh <purefkh@gmail.com>
2018-11-13 13:36:14 +0800

    逻辑优化

--- setup.sh
+++ setup.sh
@@ -9,13 +9,13 @@ case $choice in
 1)
     echo ""
     echo "You chose campus AB"
-    mv latest-wired_ab.py drcom
-    mv drcom_ab.conf drcom.conf;;
+    cp -p latest-wired_ab.py drcom
+    cp -p drcom_ab.conf drcom.conf;;
 2)
     echo ""
     echo "You chose campus D"
-    mv latest-wired_d.py drcom
-    mv drcom_d.conf drcom.conf;;
+    cp -p latest-wired_d.py drcom
+    cp -p drcom_d.conf drcom.conf;;
 *)
     echo ""
     echo "Errors! Please run the program again."
@@ -28,20 +28,37 @@ sed -i "s/password=''/password=\'$password\'/g" drcom.conf
 echo "Install python-mini..."
 opkg install zlib_1.2.8-1_ralink.ipk
 opkg install python-mini_2.7.3-2_ralink.ipk
-echo "remove python_ipk..."
-rm -rf *.ipk
-sleep 1s
 echo "Set up scripts..."
 chmod +x *.sh
 chmod +x *drcom
-mv 99-drcom /etc/hotplug.d/iface/
-mv drcom /usr/bin/
-mv networkChecking.sh /usr/bin/
-mv drcom.conf /etc/
+cp -p 99-drcom /etc/hotplug.d/iface/
+cp -p drcom /usr/bin/
+cp -p networkChecking.sh /usr/bin/
+cp -p drcom.conf /etc/
 sleep 1s
 echo "Almost done!"
 echo "Set up cron..."
 crontab mycron
 /etc/init.d/cron restart
+echo "Network checking..."
+python /usr/bin/drcom > ~/drcom.log &
+sleep 2s
+ping -c 1 baidu.com > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    echo "OK..."
+    ls
+    read -p "Remove these files... (Y /N)" option
+    case $option in
+    Y | y)
+        rm -rf ../CQU_drcom;;
+    *)
+        echo "Not remove";;
+    esac
+else
+    echo "Failed... Please contact me."
+    exit 0
+fi
 sleep 1s
 echo "It is OK. Enjoy!"
+
